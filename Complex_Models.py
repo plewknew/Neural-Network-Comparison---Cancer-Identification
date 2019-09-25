102fb9459b5c6c9321315ee44d51af17afe4bc40
Patrick Lewis <patrickianlewis@gmail.com>
2019-09-25 17:05:29 -0400

    Updated CNN with Residuals
    There was an error due to the matrixs shapes, fixed now

--- Complex_Models.py
+++ Complex_Models.py
@@ -136,11 +136,12 @@ def deep_CNN_with_Res(num_classes, input_shape_input, kernel_size_input, final_a
     
     warnings.filterwarnings("ignore")
 
-    inputs = Input(shape=input_shape_input)
+    inputs = Input(input_shape_input)
 
     conv1_1 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(inputs)
     conv1_1 = Activation("relu")(conv1_1)
-    conv1_1 = batch2 = BatchNormalization()(conv1_1)
+
+    conv1_1 = BatchNormalization()(conv1_1)
 
 
     conv1_2 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv1_1)
@@ -155,9 +156,10 @@ def deep_CNN_with_Res(num_classes, input_shape_input, kernel_size_input, final_a
     skip1 = add([conv1_1, conv1_3])
     conv1_4 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip1)
     conv1_4 = Activation("relu")(conv1_4)
-    conv1_4 = batch2 = BatchNormalization()(conv1_4)
+    conv1_4 = BatchNormalization()(conv1_4)
 
     maxpool1 = MaxPooling2D(pool_size=(2, 2))(conv1_4)
+
 
     conv2_1 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(maxpool1)
     conv2_1 = Activation("relu")(conv2_1)
@@ -168,12 +170,20 @@ def deep_CNN_with_Res(num_classes, input_shape_input, kernel_size_input, final_a
     conv2_2 = Activation("relu")(conv2_2)
     conv2_2 = batch2 = BatchNormalization()(conv2_2)
 
-    skip2 = add([maxpool1, conv2_2])
-    conv2_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip2)
+    conv2_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv2_2)
     conv2_3 = Activation("relu")(conv2_3)
     conv2_3 = batch2 = BatchNormalization()(conv2_3)
 
-    maxpool2 = MaxPooling2D(pool_size=(2, 2))(conv2_3)
+
+    skip2 = add([conv2_1, conv2_3])
+    conv2_4 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip2)
+    conv2_4 = Activation("relu")(conv2_4)
+    conv2_4 = BatchNormalization()(conv2_4)
+
+
+
+    maxpool2 = MaxPooling2D(pool_size=(2, 2))(conv2_4)
+
 
     conv3_1 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(maxpool1)
     conv3_1 = Activation("relu")(conv3_1)
@@ -184,42 +194,69 @@ def deep_CNN_with_Res(num_classes, input_shape_input, kernel_size_input, final_a
     conv3_2 = Activation("relu")(conv3_2)
     conv3_2 = batch2 = BatchNormalization()(conv3_2)
 
-    skip3 = add([maxpool2, conv3_2])
-    conv3_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip3)
+    conv3_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv3_2)
     conv3_3 = Activation("relu")(conv3_3)
     conv3_3 = batch2 = BatchNormalization()(conv3_3)
 
-    maxpool3 = MaxPooling2D(pool_size=(2, 2))(conv3_3)
+
+    skip3 = add([conv3_1, conv3_3])
+    conv3_4 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip3)
+    conv3_4 = Activation("relu")(conv3_4)
+    conv3_4 = BatchNormalization()(conv3_4)
+
+
+
+    maxpool3 = MaxPooling2D(pool_size=(2, 2))(conv3_4)
+
 
     conv4_1 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(maxpool3)
     conv4_1 = Activation("relu")(conv4_1)
     conv4_1 = batch2 = BatchNormalization()(conv4_1)
 
+
     conv4_2 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv4_1)
     conv4_2 = Activation("relu")(conv4_2)
     conv4_2 = batch2 = BatchNormalization()(conv4_2)
 
-    skip4 = add([conv4_1, conv4_2])
-    conv4_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip4)
+    conv4_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv4_2)
     conv4_3 = Activation("relu")(conv4_3)
     conv4_3 = batch2 = BatchNormalization()(conv4_3)
 
-    maxpool4 = MaxPooling2D(pool_size=(2, 2))(conv4_3)
+
+    skip4 = add([conv4_1, conv4_3])
+    conv4_4 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip4)
+    conv4_4 = Activation("relu")(conv4_4)
+    conv4_4 = BatchNormalization()(conv4_4)
+
+
+
+    maxpool4 = MaxPooling2D(pool_size=(2, 2))(conv4_4)
+
+
 
     conv5_1 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(maxpool4)
     conv5_1 = Activation("relu")(conv5_1)
     conv5_1 = batch2 = BatchNormalization()(conv5_1)
 
+
     conv5_2 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv5_1)
     conv5_2 = Activation("relu")(conv5_2)
     conv5_2 = batch2 = BatchNormalization()(conv5_2)
 
-    skip5 = add([maxpool4, conv5_2])
-    conv5_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip5)
+    conv5_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv5_2)
     conv5_3 = Activation("relu")(conv5_3)
     conv5_3 = batch2 = BatchNormalization()(conv5_3)
 
-    maxpool5 = MaxPooling2D(pool_size=(2, 2))(conv5_3)
+
+    skip5 = add([conv5_1, conv5_3])
+    conv5_4 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip5)
+    conv5_4 = Activation("relu")(conv5_4)
+    conv5_4 = BatchNormalization()(conv5_4)
+
+
+
+    maxpool5 = MaxPooling2D(pool_size=(2, 2))(conv5_4)
+
 
     conv6_1 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(maxpool5)
     conv6_1 = Activation("relu")(conv6_1)
@@ -230,21 +267,28 @@ def deep_CNN_with_Res(num_classes, input_shape_input, kernel_size_input, final_a
     conv6_2 = Activation("relu")(conv6_2)
     conv6_2 = batch2 = BatchNormalization()(conv6_2)
 
-    skip6 = add([maxpool5, conv6_2])
-    conv6_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip6)
+    conv6_3 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(conv6_2)
     conv6_3 = Activation("relu")(conv6_3)
     conv6_3 = batch2 = BatchNormalization()(conv6_3)
 
-    maxpool6 = MaxPooling2D(pool_size=(2, 2))(conv6_3)
+
+    skip6 = add([conv6_1, conv6_3])
+    conv6_4 = Conv2D(32, kernel_size=kernel_size_input, padding='same')(skip6)
+    conv6_4 = Activation("relu")(conv6_4)
+    conv6_4 = BatchNormalization()(conv6_4)
+
+
+
+    maxpool6 = MaxPooling2D(pool_size=(2, 2))(conv6_4)
+
 
     flat = Flatten()(maxpool6)
     dense = Dense(64, activation='relu')(flat)
     predictions = Dense(num_classes, activation=final_activation)(dense)
 
     model_CNN_res = Model(inputs=inputs, outputs=predictions)
-
     model_CNN_res.compile("adam", "binary_crossentropy", metrics=['accuracy'])
-
+    
     warnings.filterwarnings("default")
 
     return model_CNN_res
